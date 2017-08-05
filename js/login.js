3d553db1906ed729fc2c6243030a0a09ee33d0f6
annywu <annyxq@126.com>
2017-08-05 13:55:20 +0800

    pages

--- js/login.js
+++ js/login.js
@@ -1,27 +1,27 @@
 // var r=document.referrer;
 // 		console.log(r);
 
-$('#btn_login').on('click',function(e){
-	e.preventDefault()
-	var user=$('#user').val();
-	var pwd=$('#pwd').val();
-	console.log(user)
-	$.ajax({
-		url:'../js/login.json',
-		type:'post',
-		data:{
-			user:user,
-			pwd:pwd,
-		},
-		dateType:"json",
-		success:function(data){
-			var data=JSON.parse(data);
+// $('#btn_login').on('click',function(e){
+// 	e.preventDefault()
+// 	var user=$('#user').val();
+// 	var pwd=$('#pwd').val();
+// 	console.log(user)
+// 	$.ajax({
+// 		url:'../js/login.json',
+// 		type:'post',
+// 		data:{
+// 			user:user,
+// 			pwd:pwd,
+// 		},
+// 		dateType:"json",
+// 		success:function(data){
+// 			var data=JSON.parse(data);
 			
-			for(var i=0;i<data.length;i++){
-				if(data[i].username==user&&data[i].pwd==pwd){
-					setCookie("username",user,1)
-				}
-			}
-		}
-	})
-})
+// 			for(var i=0;i<data.length;i++){
+// 				if(data[i].username==user&&data[i].pwd==pwd){
+// 					setCookie("username",user,1)
+// 				}
+// 			}
+// 		}
+// 	})
+// })
